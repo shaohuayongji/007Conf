1d4c03ef7afb0787a5fbc9f807b41597fe414832
jpcnmm <jpcnmm@gmail.com>
2019-10-08 19:50:44 +0800

    Create weread.js

---
+++ QuanX/Scripts/weread.js
@@ -0,0 +1,16 @@
+var body = $response.body;
+let path = "/pay/memberCardSummary";
+if (url.indexOf(path) != -1){
+  var obj = JSON.parse(body);
+  obj["expiredTime"] = 1747624048;
+  obj["remainTime"] = 189302400;
+  body = JSON.stringify(obj);
+}
+$done({body}); 
+
+/*
+Made by Meeta (微信阅读)
+https?:\/\/i\.weread\.qq.com\/pay\/memberCardSummary\?pf
+hostname = i.weread.qq.com
+*/
+
